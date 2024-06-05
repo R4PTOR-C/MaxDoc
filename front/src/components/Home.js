9141c56f72726618a2d1848aa93acc36b8404dd5
rafael.caldas <rafaelcroriz7@gmail.com>
2024-06-05 10:45:33 -0300

    .

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
-    const [remedios, setRemedios] = useState([]);
+    const [lembretes, setLembretes] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         setLoading(true);
-        //maxdoc.onrender.com
-        fetch(`${process.env.REACT_APP_API_URL}/remedios`)
+        fetch(`${process.env.REACT_APP_API_URL}/lembretes`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Erro na resposta do servidor');
@@ -17,18 +19,40 @@ const Home = () => {
                 return response.json();
             })
             .then(data => {
-                setRemedios(data);
+                setLembretes(data);
                 setLoading(false);
             })
             .catch(error => {
-                console.error("Erro ao buscar dados de remedios:", error);
+                console.error("Erro ao buscar dados de lembretes:", error);
                 setError(error.toString());
                 setLoading(false);
             });
     }, []);
 
+    const handleDateClick = (arg) => {
+        console.log(arg.dateStr); // Exibe a data clicada no console
+    };
+
     return (
-        <h1>home</h1>
+        <div className="container mt-5">
+            <h1>Home</h1>
+            {loading && <p>Carregando...</p>}
+            {error && <p>Erro: {error}</p>}
+            {!loading && !error && (
+                <>
+                    <FullCalendar
+                        plugins={[dayGridPlugin, interactionPlugin]}
+                        initialView="dayGridMonth"
+                        dateClick={handleDateClick}
+                        events={lembretes.map(lembrete => ({
+                            title: lembrete.descricao,
+                            date: lembrete.data
+                        }))}
+                        weekends={true}
+                    />
+                </>
+            )}
+        </div>
     );
 };
 
