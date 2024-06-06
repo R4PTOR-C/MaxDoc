c024295229a8068bb024332ed11479df30c95e38
Rafael <rafaelcroriz7@gmail.com>
2024-06-06 13:35:26 -0300

    .

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import ptBrLocale from '@fullcalendar/core/locales/pt-br'; // Importe o pacote de localização em português
+import ptBrLocale from '@fullcalendar/core/locales/pt-br';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
     const [lembretes, setLembretes] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [proximoLembrete, setProximoLembrete] = useState(null);
 
     useEffect(() => {
         setLoading(true);
@@ -22,6 +23,7 @@ const Home = () => {
             .then(data => {
                 setLembretes(data);
                 setLoading(false);
+                encontrarProximoLembrete(data);
             })
             .catch(error => {
                 console.error("Erro ao buscar dados de lembretes:", error);
@@ -30,29 +32,50 @@ const Home = () => {
             });
     }, []);
 
+    const encontrarProximoLembrete = (lembretes) => {
+        const hoje = new Date();
+        const lembretesFuturos = lembretes.filter(lembrete => new Date(lembrete.data) >= hoje);
+        const proximo = lembretesFuturos.sort((a, b) => new Date(a.data) - new Date(b.data))[0];
+        setProximoLembrete(proximo);
+    };
+
     const handleDateClick = (arg) => {
         console.log(arg.dateStr); // Exibe a data clicada no console
     };
 
     return (
         <div className="container mt-5">
-            <h1>Home</h1>
             {loading && <p>Carregando...</p>}
             {error && <p>Erro: {error}</p>}
             {!loading && !error && (
-                <>
-                    <FullCalendar
-                        plugins={[dayGridPlugin, interactionPlugin]}
-                        initialView="dayGridMonth"
-                        locale={ptBrLocale} // Configura o idioma para português
-                        dateClick={handleDateClick}
-                        events={lembretes.map(lembrete => ({
-                            title: lembrete.descricao,
-                            date: lembrete.data
-                        }))}
-                        weekends={true}
-                    />
-                </>
+                <div className="row">
+                    <div className="col-md-4">
+                        <h2>Próximo Remédio</h2>
+                        {proximoLembrete ? (
+                            <div>
+                                <p><strong>Descrição:</strong> {proximoLembrete.descricao}</p>
+                                <p><strong>Data:</strong> {new Date(proximoLembrete.data).toLocaleDateString('pt-BR')}
+                                </p>
+                            </div>
+                        ) : (
+                            <p>Nenhum lembrete futuro encontrado.</p>
+                        )}
+                    </div>
+                    <div className="col-md-8">
+                        <FullCalendar
+                            plugins={[dayGridPlugin, interactionPlugin]}
+                            initialView="dayGridMonth"
+                            locale={ptBrLocale}
+                            dateClick={handleDateClick}
+                            events={lembretes.map(lembrete => ({
+                                title: lembrete.descricao,
+                                date: lembrete.data
+                            }))}
+                            weekends={true}
+                        />
+                    </div>
+
+                </div>
             )}
         </div>
     );
